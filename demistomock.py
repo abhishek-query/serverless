501263c387a87d62c9106c708c2950ee5e7ebd25
Kyle Putnam <kyle@query.ai>
2022-08-06 21:55:45 -0500

    Implement setattr and also ignore a false type error

--- demistomock.py
+++ demistomock.py
@@ -1,8 +1,7 @@
-
 # NOTE: This module must be loaded before any Demisto drivers are loaded.
 
 from typing import Optional
-from DemistoClassApiModule.DemistoClassApiModule import Demisto
+from Packs.ApiModules.Scripts.DemistoClassApiModule.DemistoClassApiModule import Demisto
 
 demisto: Optional[Demisto] = None
 
@@ -10,13 +9,13 @@ def setup(context: dict) -> None:
     global demisto
     demisto = Demisto(context)
 
-# NOTE: We are monkey patching `hasattr`! By creating our own "demistomock.py", we
+# We are monkey patching `hasattr`! By creating our own "demistomock.py", we
 # will be imported when drivers write "import demistomock as demisto".
 #
 # https://code.activestate.com/lists/python-list/14972
 def _hasattr(obj: object, attr: str, orig_hasattr=hasattr) -> bool:
     if orig_hasattr(obj, '__hasattr__'):
-        return obj.__hasattr__(attr)
+        return obj.__hasattr__(attr) # type: ignore
     return orig_hasattr(obj, attr)
 __builtins__['hasattr'] = _hasattr
 
@@ -25,6 +24,10 @@ __builtins__['hasattr'] = _hasattr
 def __getattr__(attr):
     if demisto is not None:
         return getattr(demisto, attr)
+
+def __setattr__(attr, value):
+    if demisto is not None:
+        return setattr(demisto, attr, value)
 
 # When something has `import demistomock as demisto`, and then does something like
 # `demisto.hasattr(...)`, we will redirect it to the value created in `setup()` above
